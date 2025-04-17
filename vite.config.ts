240ab522b7991eeba65c345dd86d9284388b4171
mathewcorfield <33930994+mathewcorfield@users.noreply.github.com>
2025-04-17 17:33:20 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   base: '/study-spark-gather/', // this must match your repo name
   plugins: [react()],
+resolve: {
+    alias: {
+      '@': '/src',  // Resolves '@' to the src directory
+    },
+  },
 });
