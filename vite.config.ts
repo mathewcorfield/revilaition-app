7fc3fcc671feee191d739437eb0e94096bbf4bd0
mathewcorfield <33930994+mathewcorfield@users.noreply.github.com>
2025-04-17 18:47:31 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,26 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
-export default defineConfig({
-  plugins: [react()],
-resolve: {
-    alias: {
-      '@': '/src',  // Resolves '@' to the src directory
-    },
+ import react from "@vitejs/plugin-react-swc";
+ import path from "path";
+ import { componentTagger } from "lovable-tagger";
+ 
+ // https://vitejs.dev/config/
+ export default defineConfig(({ mode }) => ({
+   server: {
+     host: "::",
+     port: 8080,
+   },
+   base: '/study-spark-gather/',  // Set this to the name of your repository or subdirectory
+   build: {
+    outDir: 'dist',  // Ensure the build output is placed in the 'dist' directory
   },
-});
+   plugins: [
+     react(),
+     mode === 'development' &&
+     componentTagger(),
+   ].filter(Boolean),
+   resolve: {
+     alias: {
+       "@": path.resolve(__dirname, "./src"),
+     },
+   },
+ }));
