be3106e659ef4270fa95e54803ef48d29ca94713
mathewcorfield <33930994+mathewcorfield@users.noreply.github.com>
2025-04-22 21:10:16 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import LoginPage from './pages/LoginPage'; 
+import DashboardPage from './pages/DashboardPage'; 
 
 const queryClient = new QueryClient();
 
