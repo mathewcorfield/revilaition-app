1efb59de699d8d05ba0d29a1dc2413a2b6cb43b4
mathewcorfield <33930994+mathewcorfield@users.noreply.github.com>
2025-04-22 19:47:20 +0100

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 // These variables must be defined in .env files
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL!
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY!
+const supabaseUrl = process.env.VITE_APP_SUPABASE_URL!
+const supabaseAnonKey = process.env.VITE_APP_SUPABASE_ANON_KEY!
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("Missing Supabase environment variables.")
