ea2362375732bd93bd3296af233d3f983088c3f3
mathewcorfield <33930994+mathewcorfield@users.noreply.github.com>
2025-04-29 10:48:35 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,48 +2,17 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App.tsx';
-import './index.css';
-import React from 'react';
-console.log("Before RootElement");
+
 const rootElement = document.getElementById('root');
-console.log("App initialization started");
-console.log("Root element:", rootElement);
-
-class ErrorBoundary extends React.Component<{ children: React.ReactNode }, { hasError: boolean; error: any }> {
-  constructor(props: { children: React.ReactNode }) {
-    super(props);
-    this.state = { hasError: false, error: null };
-  }
-
-  static getDerivedStateFromError(error: any) {
-    return { hasError: true, error };
-  }
-
-  componentDidCatch(error: any, errorInfo: any) {
-    console.error('React ErrorBoundary caught an error:', error, errorInfo);
-  }
-
-  render() {
-    if (this.state.hasError) {
-      return <div>Something broke: {String(this.state.error)}</div>;
-    }
-
-    return this.props.children;
-  }
-}
 
 if (rootElement) {
-  console.log("Before rendering App");
   createRoot(rootElement).render(
-    //<StrictMode>
+    <StrictMode>
       <HashRouter>
-        <ErrorBoundary>
-          <App />
-        </ErrorBoundary>
+        <App />
       </HashRouter>
-   // </StrictMode>
+    </StrictMode>
   );
-  console.log("App rendered successfully");
 } else {
   throw new Error('Root element not found');
 }
