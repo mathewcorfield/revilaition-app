94a280a60f9a6a991a6c256612901d3b4e6c1fa7
mathewcorfield <33930994+mathewcorfield@users.noreply.github.com>
2025-04-29 11:01:04 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import { Toaster } from "@/components/ui/toaster"; 
+import { Toaster as Sonner } from "@/components/ui/sonner"; 
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import SubjectPage from "@/pages/SubjectPage";
+import ProtectedRoute from '@/components/ProtectedRoute';
+import useTrackUserInteractions from "@/hooks/useTrackUserInteractions"; 
 import { UserProvider } from "@/context/UserContext";
 
 const App = () => {
+  console.log("App Component Loaded");
+
+  useTrackUserInteractions(); // Tracking user interactions
+
   return (
     <UserProvider>
       <div>
