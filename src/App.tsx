cbb65797d632699eb3a439e3aa880bc00733422d
mathewcorfield <33930994+mathewcorfield@users.noreply.github.com>
2025-04-29 10:49:02 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,8 @@
-import { Toaster } from "@/components/ui/toaster"; 
-import { Toaster as Sonner } from "@/components/ui/sonner"; 
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import SubjectPage from "@/pages/SubjectPage";
-import ProtectedRoute from '@/components/ProtectedRoute';
-import useTrackUserInteractions from "@/hooks/useTrackUserInteractions"; 
-//import { UserProvider } from "@/context/UserContext";
-
-const queryClient = new QueryClient();
-
 const App = () => {
-  console.log("App Component Loaded");
-
-  useTrackUserInteractions(); // Tracking user interactions
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster /> {/* Make sure you only need this one */}
-        <Sonner />  {/* Use this only if it's necessary */}
-        //<UserProvider>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/login" element={<Login />} />
-            //<Route path="/dashboard" element={<Dashboard />} />
-            //<Route path="/subject/:id" element={<SubjectPage />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        //</UserProvider>
-      </TooltipProvider>
-    </QueryClientProvider>
+    <div>
+      <h1>App is loading</h1>
+    </div>
   );
 };
 
