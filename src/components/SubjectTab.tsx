868860a70ab3c123d52eb62bc7e4d2613e26daa4
mathewcorfield <33930994+mathewcorfield@users.noreply.github.com>
2025-04-30 17:24:07 +0100

    Update SubjectTab.tsx

--- src/components/SubjectTab.tsx
+++ src/components/SubjectTab.tsx
@@ -11,7 +11,7 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@
 import { Subject, AvailableSubject } from "@/types";
 import SubjectDetail from "./SubjectDetail";
 import { useToast } from "@/hooks/use-toast";
-import { addUserSubject, removeUserSubject } from "@/services/dataservice";
+import { addUserSubject, removeUserSubject } from "@/services/dataService";
 
 interface SubjectTabProps {
   userId: string;
