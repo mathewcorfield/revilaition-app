829ac4dd96f4165bd183a256c60f882d5d4778b7
mathewcorfield <33930994+mathewcorfield@users.noreply.github.com>
2025-04-30 13:52:47 +0100

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,7 +1,7 @@
-
 import React from 'react';
 import { Button } from "@/components/ui/button";
 import { ArrowRight, GraduationCap, CheckCircle, LightbulbIcon, BookOpenCheck } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
 const HeroSection: React.FC = () => {
   return (
