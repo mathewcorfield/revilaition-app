201dd17107df86a210e71cb0fe3f9507b2680f6f
mathewcorfield <33930994+mathewcorfield@users.noreply.github.com>
2025-05-02 12:50:35 +0100

    Update addUserSubject.tsx

--- src/hooks/addUserSubject.tsx
+++ src/hooks/addUserSubject.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useUser } from "@/context/UserContext";
 import { addUserSubject, fetchSubtopicsForSubject } from "@/services/dataService";
 import { useToast } from "@/hooks/use-toast";
-import { Subject, AvailableSubject } from "@/types";
+import { Subject } from "@/types";
 
 export const useAddSubject = () => {
   const { user, setUser } = useUser();
@@ -10,35 +10,29 @@ export const useAddSubject = () => {
   const [isAdding, setIsAdding] = useState(false);
 
   const handleAddSubject = async (
-    selectedSubjectToAdd: string,
-    availableSubjects: AvailableSubject[],
-    selectedExamBoard: string
+    selectedSubject: Subject
   ) => {
-    if (!availableSubjects) return;
-
-    const subjectToAdd = availableSubjects.find(s => s.id === selectedSubjectToAdd);
-    
-    if (subjectToAdd && !subjectToAdd.launched) {
+    if (selectedSubject && !selectedSubject.launched) {
       toast({
         title: "Coming Soon",
-        description: `${subjectToAdd.name} is not yet available.`,
+        description: `${selectedSubject.name} is not yet available.`,
         variant: "destructive",
       });
       return;
     }
     
-    if (subjectToAdd && selectedExamBoard) {
+    if (selectedSubject) {
       try {
         setIsAdding(true); // Start loading state
         
-        await addUserSubject(user.id, subjectToAdd.id, selectedExamBoard);
-        const fetchedSubtopics = await fetchSubtopicsForSubject(subjectToAdd.id);
+        await addUserSubject(user.id, selectedSubject.id);
+        const fetchedSubtopics = await fetchSubtopicsForSubject(selectedSubject.id);
         
         const newSubject: Subject = {
-          id: subjectToAdd.id,
-          name: subjectToAdd.name,
-          examBoard: selectedExamBoard,
-          iconColor: subjectToAdd.iconColor,
+          id: selectedSubject.id,
+          name: selectedSubject.name,
+          examBoard: selectedSubject.examBoard,
+          iconColor: selectedSubject.iconColor,
           subtopics: (fetchedSubtopics || []).map(st => ({
             ...st,
             learnt: 0,
@@ -53,7 +47,7 @@ export const useAddSubject = () => {
 
         toast({
           title: "Subject Added",
-          description: `${subjectToAdd.name} has been added to your subjects.`,
+          description: `${selectedSubject.name} has been added to your subjects.`,
         });
       } catch (error) {
         toast({
