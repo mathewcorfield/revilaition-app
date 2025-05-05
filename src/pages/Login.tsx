f9b54a763767f7ea014f28c01c456c5104cc7f54
mathewcorfield <33930994+mathewcorfield@users.noreply.github.com>
2025-05-05 23:40:55 +0100

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -84,7 +84,7 @@ const Login = () => {
             email,
             password,
             options: {
-              emailRedirectTo: "https://revilaition.com/login",
+              emailRedirectTo: "https://revilaition.com/dashboard?trial=true",
             },
           });
 
