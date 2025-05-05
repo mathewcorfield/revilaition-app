ecf6f66fb3de231b4c32f3d803f189a14effd3d8
mathewcorfield <33930994+mathewcorfield@users.noreply.github.com>
2025-05-05 23:30:55 +0100

    Update useIsTrialMode.ts

--- src/hooks/useIsTrialMode.ts
+++ src/hooks/useIsTrialMode.ts
@@ -1,9 +1,10 @@
 import { useSearchParams } from 'react-router-dom';
 
-export const useIsTrialMode = (): boolean => {
+const useIsTrialMode = (): boolean => {
   const [searchParams] = useSearchParams();
   const trial = searchParams.get('trial');
 
   // Return false if trial is not 'true' or if it's missing.
   return trial === 'true';
 };
+export default useIsTrialMode;
