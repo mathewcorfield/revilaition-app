f7b988eadda949cc8a0892a8eba61f43046cc5dc
mathewcorfield <33930994+mathewcorfield@users.noreply.github.com>
2025-05-05 22:55:45 +0100

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -18,6 +18,8 @@ import { useIsTrialMode } from '@/hooks/useIsTrialMode';
 const Dashboard = () => {
   useAuthRedirect();
   const isTrial = useIsTrialMode();
+  // Debugging log for trial value
+  console.log("Trial parameter value:", isTrial);
   
   const { allSubjects, loadingSubjects } = useSubjects();
   const { allExamBoards, loadingExamBoards } = useExamBoards();
