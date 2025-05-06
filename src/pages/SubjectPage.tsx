ac1d51833169eb2ca3b568e794a9884a9a2bb711
mathewcorfield <33930994+mathewcorfield@users.noreply.github.com>
2025-05-06 09:58:58 +0100

    Update SubjectPage.tsx

--- src/pages/SubjectPage.tsx
+++ src/pages/SubjectPage.tsx
@@ -268,7 +268,7 @@ handleGenerateQuestion(random.name, random);}
             </Button>
   </div>
     </div>
-    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+    <div className="grid gap-4 grid-cols-1">
   {subtopics.map((subtopic) => (
     <SubtopicCard
             key={subtopic.id}
