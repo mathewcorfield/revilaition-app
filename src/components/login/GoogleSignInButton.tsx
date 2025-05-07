191118220c74a5bdc5de594886bbdcdfeda0c366
mathewcorfield <33930994+mathewcorfield@users.noreply.github.com>
2025-05-07 13:05:37 +0100

    Update GoogleSignInButton.tsx

--- src/components/login/GoogleSignInButton.tsx
+++ src/components/login/GoogleSignInButton.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { FaGoogle } from "react-icons/fa";
-import { useGoogleSignIn } from "@/hooks/useGoogleSignIn";
+import useGoogleSignIn from "@/hooks/useGoogleSignIn";
 
 const GoogleSignInButton = () => {
   const { handleGoogleSignIn, loading } = useGoogleSignIn();
