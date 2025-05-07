ac03cac941fd23aa81934d82fed4b5c14946f960
mathewcorfield <33930994+mathewcorfield@users.noreply.github.com>
2025-05-07 12:49:23 +0100

    Update GoogleSignInButton.tsx

--- src/components/login/GoogleSignInButton.tsx
+++ src/components/login/GoogleSignInButton.tsx
@@ -4,15 +4,15 @@ import { FaGoogle } from "react-icons/fa";
 import { useGoogleSignIn } from "@/hooks/useGoogleSignIn";
 
 const GoogleSignInButton = () => {
-  const { handleGoogleSignIn, googleLoading } = useGoogleSignIn();
+  const { handleGoogleSignIn, loading } = useGoogleSignIn();
   return (
     <Button
       onClick={handleGoogleSignIn}
       className="w-full mt-4 flex items-center justify-center"
-      disabled={googleLoading}
+      disabled={loading}
     >
       <img src="/google.webp" alt="Google Logo" className="w-6 h-6 mr-2" />
-      {googleLoading ? "Signing in with Google..." : "Sign in with Google"}
+      {loading ? "Signing in with Google..." : "Sign in with Google"}
     </Button>
   );
 };
